f55707b44416180ccfdd8231126940e7893c2866
Damián Jiménez <damianjimenez9@gmail.com>
2020-04-28 10:58:47 +0200

    Agregamos y modificamos módulos y rutas en Pages y Shared

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Custom modules
+import { SharedModule } from '../shared/shared.module';
+
+// Pages
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+// Routes
+import { PAGES_ROUTES } from './pages.routes';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    PAGES_ROUTES
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent
+  ]
+})
+export class PagesModule { }
